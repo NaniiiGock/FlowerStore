fdda36e35b9564ef415ae489e3188045b67ab485
NaniiiGock <liliana.hotsko@ucu.edu.ua>
2022-10-20 15:08:29 +0300

    Commit

--- src/main/java/flower/store/FlowerBucket.java
+++ src/main/java/flower/store/FlowerBucket.java
@@ -1,9 +1,13 @@
 package flower.store;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class FlowerBucket {
+    @Getter @Setter
     private List<FlowerPack> bucket = new ArrayList<>();
 
     public void add(FlowerPack pack) {
