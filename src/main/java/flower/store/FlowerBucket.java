1941d40eee0d7dff42dfde54ca026a658672dc3f
NaniiiGock <liliana.hotsko@ucu.edu.ua>
2022-10-20 15:04:20 +0300

    Initial commit

---
+++ src/main/java/flower/store/FlowerBucket.java
@@ -0,0 +1,20 @@
+package flower.store;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlowerBucket {
+    List<FlowerPack> bucket = new ArrayList<>();
+
+    public void add(FlowerPack pack) {
+        bucket.add(pack);
+    }
+
+    public double getPrice() {
+        double price = 0;
+        for (FlowerPack pack : bucket) {
+            price += pack.getPrice();
+        }
+        return price;
+    }
+}
